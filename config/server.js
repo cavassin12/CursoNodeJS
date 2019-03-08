10a97df3418b1cd4deacb5e262176a7a02972903
Vanderlei Cavassin <suporte@salsibox.com.br>
2019-03-08 17:41:13 -0300

    Commit de preparo para desenvolvimento

---
+++ config/server.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var consign = require('consign');
+var body = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views/');
+
+app.use(body.urlencoded({extended:true}));
+
+consign()
+    .include('app/rotas')
+    .then('app/models')
+    .into(app);
+
+
+    module.exports = app;
+
+
