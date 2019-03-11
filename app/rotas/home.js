a5526f67d5ce896bb97e2c48cf39392e341e9119
Vanderlei Cavassin <suporte@salsibox.com.br>
2019-03-11 15:54:40 -0300

    commit novo branch da session

--- app/rotas/home.js
+++ app/rotas/home.js
@@ -1,38 +1,42 @@
-module.exports = function(appl){
+module.exports = function (appl) {
 
     var md5 = require('md5');
 
-    appl.get('/', function(req, res){
+    appl.get('/', function (req, res) {
+        /*if(req.session.logado == true){
+            console.log('não logado');
+        }*/
         res.render('home/index');
     });
-    appl.get('/Login', function(req, res){
+    appl.get('/Login', function (req, res) {
         res.render('home/login');
     });
-    appl.all('/Logar', function(req, res){
+    appl.all('/Logar', function (req, res) {
         var dados = req.body;
         var v_login = dados.login;
         var v_senha = dados.senha;
 
-        if(v_login.length == 0){
+        if (v_login.length == 0) {
             res.redirect('/Login?error=user');
         }
-        if(v_senha.length == 0){
+        if (v_senha.length == 0) {
             res.redirect('/Login?error=senha');
-        }else{
+        } else {
             v_senha = md5(dados.senha);
             //console.log(v_senha);
         }
-        
+
         let conna = appl.config.dbconn();
         var logi = new appl.app.models.DaoLogin(conna);
 
-        logi.getLogar(v_login, v_senha, function(error, resulta){
-            if(error){
+        logi.getLogar(v_login, v_senha, function (error, resulta) {
+            if (error) {
                 console.log("Erro: " + error.message)
-            }else{
-                if(resulta.length > 0){
-                        res.redirect('/');
-                }else{
+            } else {
+                if (resulta.length > 0) {
+                  //  req.session.logado = true;
+                    res.redirect('/');
+                } else {
                     console.log("nao a registros");
                     res.redirect('/Login?error=ine');
                 }
@@ -41,5 +45,5 @@ module.exports = function(appl){
         });
 
     });
-    
+
 }
